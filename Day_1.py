63439e7f3ae7419ec2c13d0b083882f15bd4ae87
Viraj C <virajchintakindi@gmail.com>
2026-07-28 23:54:41 +0530

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,36 @@
+# Excercises Day-1
+
+print("python --version")
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+x="Viraj"
+y="Family"
+z="India"
+a="I am enjoying 30 days of python"
+
+print(x)
+print(y)
+print(z)
+print(a)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Viraj"))
+print(type('Family'))
+print(type("India"))
+
+# Exercises: Level 2
+
+
+
+
---
+++ helloworld.py
@@ -0,0 +1,44 @@
+# Excercises Day-1
+
+print("python --version")
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+x="Viraj"
+y="Family"
+z="India"
+a="I am enjoying 30 days of python"
+
+print(x)
+print(y)
+print(z)
+print(a)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Viraj"))
+print(type('Family'))
+print(type("India"))
+
+# Exercises: Level 2
+
+
+# Exercises: Level 3
+
+(x1,x2)=(2,3)
+(y1,y2)=(10,8)
+
+print((y2-y1),(x2-x1))
+
+
+
+
