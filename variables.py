7dbf67285de44184dd704391cb554302a926de88
Viraj C <virajchintakindi@gmail.com>
2026-07-29 23:08:27 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,69 @@
+# Day 2: 30 Days of python programming
+
+first_name = "Viraj"
+last_name = "Family"
+full_name = first_name + " " + last_name
+country="India"
+city="Pune"
+age=20
+year=2026
+is_married="No"
+is_true=True
+is_light_on="Yes"
+multiple_variable = first_name + last_name + full_name + country
+
+
+# Excercises : Level 2
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(multiple_variable))
+print(type(first_name))
+
+print(len(first_name))
+
+print(len(first_name), len(last_name))
+
+num_one=5
+num_two=4
+total=num_one+num_two
+diff=num_two-num_one
+product=num_two*num_one
+division=num_one/num_two
+remainder=num_two%num_one
+exp=num_one**num_two
+floor_division=num_one//num_two
+radius=30
+area_of_circle=3.14*radius**2
+circum_of_circle=2*3.14*radius
+rad=int(input("Enter radius of circle: "))
+area_of_circle_1=3.14*rad**2
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+country=input("Enter your country: ")
+age=int(input("Enter your age: "))
+
+print(total)
+print(diff)
+print(product)
+print(division)
+print(remainder)
+print(exp)
+print(floor_division)
+print(area_of_circle)
+print(circum_of_circle)
+print(area_of_circle_1)
+
+print("First Name: ", first_name)
+print("Last Name: ", last_name)
+print("Country: ", country)
+print("Age: ", age)
