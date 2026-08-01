ffac58fa6bee8acbba4f429d11dfd5f410bc7d9c
Viraj C <virajchintakindi@gmail.com>
2026-08-01 23:42:31 +0530

    Add files via upload

---
+++ Day_4.py
@@ -0,0 +1,88 @@
+# Excercises Day 4
+
+from gettext import find
+
+
+a="Thirty"
+b="Days"
+c="Of"
+d="Python"
+print(a+" "+b+" "+c+" "+d)
+
+x="Coding"
+y="For"
+z="All"
+print(x+" "+y+" "+z)
+
+company='Coding For All'
+
+print(company)
+
+print(len(company))
+
+print(company.upper())
+
+print(company.lower())
+
+print(company.capitalize())
+
+print(company.title())
+
+print(company.swapcase())
+
+print(company[7::])
+
+if 'Coding' in company:
+    print("True")
+
+print(company.replace('Coding', 'Python'))
+
+print(company.replace('Python for Everyone', 'Python for All'))
+
+print(company.split())
+
+a="Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+
+print(a.split())
+
+print(company[0])
+
+print(company[-1])
+
+print(company[10])
+
+PFE="Python For Everyone"
+CFA="Coding For All"
+
+print(company[0])
+print(company[7])
+print(company[10])
+
+b="You cannot end a sentence with because because because is a conjunction"
+print(b.find('because'))
+
+c="You cannot end a sentence with because because because is a conjunction"
+print(c.rindex('because'))
+
+print(b.strip('because'))
+
+print(find('because'))
+
+print(company.startswith('Coding'))
+
+print(company.endswith('Coding'))
+
+print(company.strip())
+
+# 31 to 35
+
+
+
+
+
+
+
+
+
+
+
